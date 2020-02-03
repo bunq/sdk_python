75a976d95b1b4541cf07604f16b6ab7e2571ad29
Angelo Melonas <angelomelonas@gmail.com>
2020-02-03 16:50:52 +0100

    feature/python-sdk-psd2: Added an internal version of ServiceProviderCredential for PSD2 context.

---
+++ bunq/sdk/model/core/PaymentServiceProviderCredentialInternal.py
@@ -0,0 +1,30 @@
+from bunq.sdk.context.api_context import ApiContext
+from bunq.sdk.http.api_client import ApiClient
+from bunq.sdk.json import converter
+from bunq.sdk.model.generated.endpoint import PaymentServiceProviderCredential, UserCredentialPasswordIp
+
+
+class PaymentServiceProviderCredentialInternal(PaymentServiceProviderCredential):
+
+    @classmethod
+    def create_with_api_context(cls,
+                                client_payment_service_provider_certificate: str,
+                                client_payment_service_provider_certificate_chain: str,
+                                client_public_key_signature: str,
+                                api_context: ApiContext,
+                                all_custom_header=None) -> UserCredentialPasswordIp:
+        request_map = {
+            cls.FIELD_CLIENT_PAYMENT_SERVICE_PROVIDER_CERTIFICATE: client_payment_service_provider_certificate,
+            cls.FIELD_CLIENT_PAYMENT_SERVICE_PROVIDER_CERTIFICATE_CHAIN: client_payment_service_provider_certificate_chain,
+            cls.FIELD_CLIENT_PUBLIC_KEY_SIGNATURE: client_public_key_signature
+        }
+
+        if all_custom_header is None:
+            all_custom_header = {}
+
+        api_client = ApiClient(api_context)
+        request_bytes = converter.class_to_json(request_map).encode()
+        endpoint_url = cls._ENDPOINT_URL_CREATE
+        response_raw = api_client.post(endpoint_url, request_bytes, all_custom_header)
+
+        return UserCredentialPasswordIp.from_json(response_raw.body_bytes.decode())
